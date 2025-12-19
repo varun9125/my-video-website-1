b19b21d0160e1355615f15bcce3302712c3aa479
varun9125 <51366919+varun9125@users.noreply.github.com>
2025-12-19 11:16:09 +0530

    Use environment variable for server port

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // 🔐 ADMIN PASSWORD (यहीं change करना)
 const ADMIN_PASSWORD = "ravi@1234";
